b39e371d4a4fdeae3dec910bd49340fb76e9b50d
JBH <xz7840@dgu.edu>
2023-12-19 16:40:49 +0900

    fix : 구글 로그인 로직

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework import routers
 from . import views
 from .views import UserBankViewSet, UserViewSet, UserDateViewSet
 
@@ -7,7 +6,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('google/login', views.google_login, name='google_login'),
-    path('google/callback/', views.google_callback, name='google_callback'),  
+    # path('google/callback/', views.google_callback, name='google_callback'),  
+    path('google/callback/', views.GoogleAccessView.as_view(), name='googel_access'),
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
     path('bank', UserBankViewSet.as_view(), name='bank'),
     path('mypage', UserViewSet.as_view(), name='mypage'),
